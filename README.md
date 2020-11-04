bd582101fcd22f7a4590e75a03647d133574f1ef
Nguyen Thanh <ndthanh20@gmail.com>
2020-11-05 00:29:31 +0700

    Create Vue project and initialize menu

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# Website_Real_Estate_Final_DBMS_Project
+# dbms_project
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/views/About.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-nav tabs align="center">
+      <b-nav-item active>Tìm mua</b-nav-item>
+      <b-nav-item>Tìm thuê</b-nav-item>
+      <b-nav-item>Tin tức</b-nav-item>
+      <b-nav-item disabled>Đăng tin</b-nav-item>
+
+      <b-nav>
+        <b-nav-item>Đăng nhập </b-nav-item>
+        |
+        <b-nav-item>Đăng kí </b-nav-item>
+      </b-nav>
+    </b-nav>
+  </div>
+</template>
